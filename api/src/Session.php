6e4bca3a6063c09f6865aef3b18f59c8449b782e
Silvan van Veen <silvtheonlyone@silvans-macbook-pro.home>
2022-03-08 22:21:40 +0100

    delete Session login/logout logic

--- api/src/Session.php
+++ api/src/Session.php
@@ -8,24 +8,4 @@ class Session {
         session_start();
     }
 
-    public static function exists($key) : bool {
-        return isset($_SESSION[$key]);
-    }
-
-    public static function get($key) : string|null {
-        
-        if (self::exists($key)) {
-            return $_SESSION[$key];
-        }
-
-        return null;
-    }
-
-    public static function set($key, $value) {
-        $_SESSION[$key] = $value;
-    }
-
-    public static function delete($key) {
-        unset($_SESSION[$key]);
-    }
 }
